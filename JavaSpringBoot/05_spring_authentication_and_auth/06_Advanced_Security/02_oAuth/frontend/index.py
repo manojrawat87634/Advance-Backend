65da333a99d7300dc71d90ede260a7a421a85cb0
manojrawat87634 <manojrawat87634@gmail.com>
2026-06-27 10:06:08 +0530

    rate limiter v2

--- JavaSpringBoot/05_spring_authentication_and_auth/06_Advanced_Security/02_oAuth/frontend/index.py
+++
@@ -1,9 +0,0 @@
-# add = lambda x, y : x + y
-# print(add(1, 2))
-
-students = [("Rahul", 78), ("Anita", 92), ("Karan", 85), ("Neha", 88)]
-students.sort(key=lambda x: x[1])
-print(students)
-
-i = [{"" : ""}]
-print()
--- JavaSpringBoot/05_spring_authentication_and_auth/06_Advanced_Security/02_oAuth/frontend/src/Component/generateInitialValues.jsx
+++
@@ -1,9 +0,0 @@
-const genrateInitalValues = (inputArr)=>{
-    let initalValues = {}
-    inputArr.forEach(element => {
-          initalValues[element.name] = element.value
-    });
-    return initalValues;
-}
-
-export default genrateInitalValues;
