6b788c86e4a3678fd2e880fd7839954cc6e80935
manojrawat87634 <manojrawat87634@gmail.com>
2026-06-29 10:11:26 +0530

    leaky bucket

---
+++ JavaSpringBoot/07_rate_limiter/03_rate_limiting_algorithms/01_leaky_bucket/src/main/java/com/example/demo/config/RateLimitFilter.java
@@ -0,0 +1,49 @@
+// package com.example.demo.config;
+
+// import java.io.IOException;
+// import java.time.Duration;
+
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.filter.OncePerRequestFilter;
+
+// import com.example.demo.services.redis.RateLimitService;
+
+// import jakarta.servlet.FilterChain;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+
+// @Component
+// public class RateLimitFilter extends OncePerRequestFilter {
+
+//     private final RateLimitService rateLimitService;
+
+//     public RateLimitFilter(RateLimitService rateLimitService) {
+//         this.rateLimitService = rateLimitService;
+//     }
+
+//     @Override
+//     protected void doFilterInternal(
+//             HttpServletRequest request,
+//             HttpServletResponse response,
+//             FilterChain filterChain)
+//             throws ServletException, IOException {
+//         String path = request.getRequestURI();
+//         // LOGIN RATE LIMIT
+//         if (path.equals("/auth/login")) {
+//             String ip = request.getRemoteAddr();
+
+//             boolean allowed = rateLimitService.isAllowed(
+//                     "rl:login:ip:" + ip,
+//                     5,
+//                     Duration.ofMinutes(10));
+
+//             if (!allowed) {
+//                 response.setStatus(429);
+//                 response.getWriter().write("Too many login attempts");
+//                 return;
+//             }
+//         }
+//         filterChain.doFilter(request, response);
+//     }
+// }
